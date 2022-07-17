0168e8e747dd8bba91fe51796bc364978c9b2da4
Aral Muftuoglu <aralkmuftuoglu@gmail.com>
2022-07-16 23:06:28 -0700

    Initial Commit

---
+++ TodoListDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoListDemoApp
+//
+//  Created by Aral Muftuoglu on 7/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoListDemoApp/TodoListDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListDemoAppApp.swift
+//  TodoListDemoApp
+//
+//  Created by Aral Muftuoglu on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
